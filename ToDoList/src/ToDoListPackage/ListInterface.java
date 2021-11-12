a68b235e0d2238bbac5927b2cb51b8332817255f
Jacob Rausch <rauschj@wit.edu>
2021-11-12 10:30:01 -0500

    Added interfaces

---
+++ ToDoList/src/ToDoListPackage/ListInterface.java
@@ -0,0 +1,5 @@
+package ToDoListPackage;
+
+public interface ListInterface {
+
+}
--- ToDoList/src/ToDoListPackage/MainGameClass.java
+++ ToDoList/src/ToDoListPackage/MainGameClass.java
@@ -4,7 +4,7 @@ public class MainGameClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test4");
+		
 	}
 
 }
