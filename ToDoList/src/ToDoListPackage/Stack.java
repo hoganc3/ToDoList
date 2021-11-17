10257288a799672560c9ec1c342370101f2e8d21
Jacob Rausch <rauschj@wit.edu>
2021-11-17 18:53:59 -0500

    task class

--- ToDoList/src/ToDoListPackage/Stack.java
+++ ToDoList/src/ToDoListPackage/Stack.java
@@ -1,6 +1,36 @@
 package ToDoListPackage;
 
-public class Stack {
+public class Stack<T> implements StackInterface{
+
+	@Override
+	public void push(Object newEntry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 
    
 }
--- ToDoList/src/ToDoListPackage/task.java
+++ ToDoList/src/ToDoListPackage/task.java
@@ -17,39 +17,58 @@ public class task {
 	}
 	
 	private double StringToDouble() {
-		char[] charToString = new char[timeLength];
-		String timeHours = "";
-		String timeMin = "";
+		String timeHours = new String("");
+		String timeMin = new String("");
 		
-		//gets rid of colon
-		boolean min = false; //still looking at hours, true if looking at minutes
-		for(int i=0; i<time.length(); i++) {
-			if(charToString[i]==':') {
-				min = true; 
-			}else {
-				if(min = true) {
-					timeMin += charToString;
-				}
-				timeHours += charToString;
-			}
-			
+		//finds where the colon is in the String
+		int colonPlace = 0;
+		if(time.charAt(1)==':') {
+			colonPlace = 1;
 		}
-		double timeH = Double.parseDouble(timeHours);
-		double timeM = Double.parseDouble(timeMin);
+		else if(time.charAt(2)==':'){
+				colonPlace = 2;
+		}
 		
+		//for loop to make String containing just the hours (before the colon)
+		for(int i=0; i<colonPlace; i++) {
+			timeHours += String.valueOf(time.charAt(i));
+		}
+		
+		//for loop to make String containing just the minutes (after the colon)
+		for(int i=colonPlace+1; i<time.length(); i++) {
+			timeMin += String.valueOf(time.charAt(i));
+		}
+		
+		//Changes the two Strings into integers
+		int timeMinInt = Integer.valueOf(timeMin);
+		int timeHoursInt = Integer.valueOf(timeHours);
+		
+		//Then changes from an integer to a decimal
+		double timeM = (double) timeMinInt;
+		System.out.println("This is timeM "+timeM);
+		double timeH = (double) timeHoursInt;
+		System.out.println("This is timeH "+timeH);
+		
+		//If its pm then 12 is added to the hours
 		if(AmPm == false) {
 			timeH += 12;
 		}
 		
+		//Min are put into a decimal form ex) 30 min is 0.5 of an hour
 		timeM = timeM / 60;
 		
+		//The two values(hours and minutes) are combined and returned
 		double totTime = timeH+timeM;
 		return totTime;
 	}
 	
+	
+		//Calls the String to Double method
 	public String sortTime() {
 		double totTime = StringToDouble();
+		//Turns into a decimal form so that there are no double digits so that it can be sorted
 		totTime = totTime / 10;
+		//Turns back to a String to be sorted
 		String newTime = Double.toString(totTime);
 		return newTime;
 	}
@@ -62,7 +81,7 @@ public class task {
 		return time;
 	}
 	
-	public void setTime(boolean AmPm) {
+	public void setAmPm(boolean AmPm) {
 		this.AmPm = AmPm;
 	}
 	public boolean getAmPm() {
